e393d44aa8d0c09cda31b05903aaf9c4d0b86d31
James Fryman <james@frymanet.com>
2012-11-16 16:36:19 -0600

    fixing logic to generate modules.pp

--- rake/puppet.rb
+++ rake/puppet.rb
@@ -1,18 +1,18 @@
 require 'yaml'
 
 nodes = YAML::load(File.open(NODES_FILE))
-modules_path = nodes['puppetmaster']['mounts']['puppet_modules']['host']
-manifests_path = nodes['puppetmaster']['mounts']['puppet_manifests']['host']
+modules_path = File.expand_path(nodes['puppetmaster']['mounts']['puppet_modules']['host'])
+manifests_path = File.expand_path(nodes['puppetmaster']['mounts']['puppet_manifests']['host'])
 
 namespace :puppet do
   namespace :generate do
     desc "Generate modules.pp while refactoring for autoloading"
     task :modules do
       begin
-        File.open(File.expand_path("#{manifests_path}/modules.pp"), 'w+') do |file|
+        File.open("#{manifests_path}/modules.pp", 'w') do |file|
           Dir[("#{modules_path}/*/manifests/init.pp")].entries.each do |path|
-            module_name = path.split('/')[1]
-            file.puts "import '#{module_name}'"
+            module_name = path.split('/')
+            file.puts "import '#{module_name[module_name.size - 3]}'"
           end
         end
       rescue => e
