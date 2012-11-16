955a40ef4bab26b463b659e711579ffdd485a524
James Fryman <james@frymanet.com>
2012-11-16 15:31:58 -0600

    fixing write status of modules.pp rake task

--- rake/puppet.rb
+++ rake/puppet.rb
@@ -8,11 +8,15 @@ namespace :puppet do
   namespace :generate do
     desc "Generate modules.pp while refactoring for autoloading"
     task :modules do
-      File.open(File.expand_path("#{manifests_path}/modules.pp", 'w')) do |file|
-        Dir[("#{modules_path}/*/manifests/init.pp")].entries.each do |path|
-          module_name = path.split('/')[1]
-          file.puts "import '#{module_name}'"
+      begin
+        File.open(File.expand_path("#{manifests_path}/modules.pp"), 'w+') do |file|
+          Dir[("#{modules_path}/*/manifests/init.pp")].entries.each do |path|
+            module_name = path.split('/')[1]
+            file.puts "import '#{module_name}'"
+          end
         end
+      rescue => e
+        puts e
       end
     end
   end
