9865700eef46af917935ebc6d90419f2f479a608
James Fryman <james@frymanet.com>
2012-12-03 12:52:31 -0600

    adding puppetdb to stroller

---
+++ bootstrap/puppetmaster/modules/repositories/manifests/init.pp
@@ -0,0 +1,8 @@
+class repositories {
+  apt::source { 'puppetlabs':
+    location   => 'http://apt.puppetlabs.com',
+    repos      => 'main',
+    key        => '4BD6EC30',
+    key_server => 'pgp.mit.edu',
+  }
+}
