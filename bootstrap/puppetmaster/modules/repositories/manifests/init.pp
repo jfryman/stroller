4c0ba0252be2237830bdae932b4a151c90c70db4
James Fryman <james@frymanet.com>
2012-12-03 19:37:16 -0600

    yeah, don't need this

--- bootstrap/puppetmaster/modules/repositories/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class repositories {
-  apt::source { 'puppetlabs':
-    location   => 'http://apt.puppetlabs.com',
-    repos      => 'main',
-    key        => '4BD6EC30',
-    key_server => 'pgp.mit.edu',
-  }
-}
